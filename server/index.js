5325411508df0a9cffeb34a63971d7ff294f147a
rajukumar1719 <rajukumar1719@gmail.com>
2026-08-31 00:20:30 +0530

    Enable CORS for deployed frontend

--- server/index.js
+++ server/index.js
@@ -5,6 +5,17 @@ import { studySchema, validateStudySession } from "./schema.js";
 dotenv.config();
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 app.use(express.json({ limit: "32kb" }));
 
 const PORT = Number(process.env.PORT || 3001);
